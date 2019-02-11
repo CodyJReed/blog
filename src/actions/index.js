034639747b1d1a8daa3a94d4ba3d743d58089d29
Cody Reed <cody2j2reed@gmail.com>
2019-02-11 13:06:04 -0500

    Added lodash, and stored unique payload to fetchUser action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,24 @@
+import _ from "lodash";
 import jsonPlaceholder from "./apis/jsonPlaceholder";
+
+/*
+fetchPostsAndUsers is... 
+Another way of eliminating mutiple action creator events
+from firing on componentDidMount() in the UserHeader Component.
+UserId is an array of unique user ID's,
+gathered by mapping over the original store, discarding any copied IDs, then call fetchUser();
+passing in the remaining IDs.
+*/
+// const fetchPostsAndUsers = () => async (dispatch, getState) => {
+//   await dispatch(fetchPosts());
+//
+//   const userId = _.chain(getState().posts)
+//     .map("userId")
+//     .uniq()
+//     .forEach(id => dispatch(fetchUser(id)))
+//     .value();
+//
+// };
 
 const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
@@ -9,13 +29,15 @@ const fetchPosts = () => async dispatch => {
   });
 };
 
-const fetchUser = id => async dispatch => {
+const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({
     type: "FETCH_USER",
     payload: response.data
   });
-};
+});
 
 export { fetchPosts, fetchUser };
